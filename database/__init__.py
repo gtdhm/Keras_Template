30c17f867485c5a1198669ad86f84959b1f968dd
GT <1023343473@qq.com>
2019-11-18 18:34:56 +0800

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -21,7 +21,7 @@ def load_database(cfg):
         test_loader: a test object of DataLoader
     """
     if cfg.mode == 'Train':
-        train_db = BaseDataset(cfg)
+        train_db = BaseDataset(cfg, use_trans=True)
         train_db.load_data(mode='Train', shuffle=True)
         train_loader = DataLoader(database=train_db, cfg=cfg)
 
